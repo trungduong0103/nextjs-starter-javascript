e03c4eee63d33fd7aacf4e60620dded6c0e9def9
trungduong0103 <trungduong0103@gmail.com>
2021-06-09 22:48:31 +0700

    messing with getStaticProps()

--- pages/posts/second-post.js
+++ pages/posts/second-post.js
@@ -1,3 +1,32 @@
-const SecondPost = () => <h1>This is the second post!</h1>;
+import Head from "next/head";
+import Layout from "../../components/layout/Layout";
+
+export async function getStaticProps() {
+  const request = await fetch(
+    "https://goweather.herokuapp.com/weather/BienHoa"
+  );
+
+  const weather = await request.json();
+
+  return {
+    props: {
+      weather: weather,
+    },
+  };
+}
+
+export function SecondPost({ weather }) {
+  return (
+    <Layout>
+      <Head>
+        <title>Weather!</title>
+      </Head>
+      <h1>Current weather for Bien Hoa:</h1>
+      <p>Temperature: {weather.temperature}</p>
+      <p>Description: {weather.description}</p>
+      <p>Wind: {weather.wind}</p>
+    </Layout>
+  );
+}
 
 export default SecondPost;
